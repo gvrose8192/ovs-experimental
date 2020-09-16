fbb2b3f0dd3f9b4ea4adbec2300866b108594183
Greg Rose <gvrose8192@gmail.com>
2020-09-16 11:42:23 -0700

    datapath: Make metadata_dst tunnel work in IP_TUNNEL_INFO_BRIDGE mode
    Upstream commit:
        commit 18b6f717483a835fb98de9f0df6c724df9324e78
        Author: wenxu <wenxu@ucloud.cn>
        Date:   Thu Mar 28 12:43:23 2019 +0800
    
        openvswitch: Make metadata_dst tunnel work in IP_TUNNEL_INFO_BRIDGE mode
    
        There is currently no support for the multicast/broadcast aspects
        of VXLAN in ovs. In the datapath flow the tun_dst must specific.
        But in the IP_TUNNEL_INFO_BRIDGE mode the tun_dst can not be specific.
        And the packet can forward through the fdb table of vxlan devcice. In
        this mode the broadcast/multicast packet can be sent through the
        following ways in ovs.
    
        ovs-vsctl add-port br0 vxlan -- set in vxlan type=vxlan \
                options:key=1000 options:remote_ip=flow
        ovs-ofctl add-flow br0 in_port=LOCAL,dl_dst=ff:ff:ff:ff:ff:ff, \
                action=output:vxlan
    
        bridge fdb append ff:ff:ff:ff:ff:ff dev vxlan_sys_4789 dst 172.168.0.1 \
                src_vni 1000 vni 1000 self
        bridge fdb append ff:ff:ff:ff:ff:ff dev vxlan_sys_4789 dst 172.168.0.2 \
                src_vni 1000 vni 1000 self
    
        Signed-off-by: wenxu <wenxu@ucloud.cn>
        Acked-by: Pravin B Shelar <pshelar@ovn.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Cc: wenxu <wenxu@ucloud.cn>
    Signed-off-by: Greg Rose <gvrose8192@gmail.com>

--- datapath/linux/compat/include/linux/openvswitch.h
+++ datapath/linux/compat/include/linux/openvswitch.h
@@ -405,6 +405,7 @@ enum ovs_tunnel_key_attr {
 	OVS_TUNNEL_KEY_ATTR_IPV6_DST,		/* struct in6_addr dst IPv6 address. */
 	OVS_TUNNEL_KEY_ATTR_PAD,
 	OVS_TUNNEL_KEY_ATTR_ERSPAN_OPTS,	/* struct erspan_metadata */
+	OVS_TUNNEL_KEY_ATTR_IPV4_INFO_BRIDGE,	/* No argument. IPV4_INFO_BRIDGE mode.*/
 	OVS_TUNNEL_KEY_ATTR_GTPU_OPTS,		/* struct gtpu_metadata */
 	__OVS_TUNNEL_KEY_ATTR_MAX
 };
