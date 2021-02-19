5b614fe60bf9fdb8a73f297329494da1e457a705
Greg Rose <gvrose8192@gmail.com>
2021-02-19 10:40:36 -0800

    compat: Provide fixups so RHEL 7.0/7.1 can compile
    Add fixups to allow build of RHEL 7.0 and 7.1
    
    Signed-off-by: Greg Rose <gvrose8192@gmail.com>

--- datapath/linux/compat/include/linux/mm.h
+++ datapath/linux/compat/include/linux/mm.h
@@ -10,6 +10,9 @@ extern void *vmalloc_node(unsigned long size, int node);
 #else
 extern void *kvmalloc_node(size_t size, gfp_t flags, int node);
 #endif /* HAVE_KVMALLOC_NODE */
+#ifndef __GFP_ZERO
+#include <linux/gfp.h>
+#endif
 static inline void *kvmalloc(size_t size, gfp_t flags)
 {
 	return kvmalloc_node(size, flags, NUMA_NO_NODE);
@@ -38,7 +41,22 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 	return kvmalloc_array(n, size, flags | __GFP_ZERO);
 }
 
-#endif
 #include_next <linux/mm.h>
+#ifndef GENMASK
+#define GENMASK(h, l) \
+	(((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+#endif
+#ifndef HAVE_KVFREE
+#include <linux/slab.h>
+static inline void rpl_kvfree(const void *addr)
+{
+	if (is_vmalloc_addr(addr))
+		vfree(addr);
+	else
+		kfree(addr);
+}
+#define kvfree rpl_kvfree
+#endif
+#endif
 #endif /* OVS_MM_H */
 
